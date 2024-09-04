34baf0000ac690744250a765fb488cbf4fa02c57
Keith Bui <kqbui@csu.fullerton.edu>
2024-09-03 20:41:47 -0700

    fixed 1-based logic error, fixed iterative issue, did some formatting

--- fib.c
+++ fib.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 
+int fibonacciIterative(int iterativeValue) {
+   int sumOfFibonacci = 0;
+   int previousNum = 0;
+   int currentNum = 1;
+   for (size_t indexOfLoop = 0; indexOfLoop < iterativeValue; ++indexOfLoop) {
+      if (indexOfLoop == 0) { continue; } // 0 does nothing
+      sumOfFibonacci = previousNum + currentNum;
+      previousNum = currentNum;
+      currentNum = sumOfFibonacci;
+   }
+   return sumOfFibonacci;
+}
+
 int fibonacciRecursive(int recursionValue) {
    if (recursionValue == 0 || recursionValue == 1) {
       return recursionValue;
    }
-   return (fibonacciRecursive(recursionValue - 2) + fibonacciRecursive(recursionValue - 1));
+   return (fibonacciRecursive(recursionValue - 2)
+    + fibonacciRecursive(recursionValue - 1));
 }
 
 int main(int argc, char *argv[]) {
-   int firstHalfOfN = *argv[1];
+   int firstHalfOfN = *argv[1] - '0'; // account for ascii from command line
    int secondHalfOfN = 0;
    char methodOfAddition = *argv[2];
    char *fileName = argv[3];
 
    FILE * fibonacciFile = fopen(fileName, "r");
-   fscanf(fibonacciFile, "%u", &secondHalfOfN);
+   fscanf(fibonacciFile, "%d", &secondHalfOfN);
    fclose(fibonacciFile);
+   int finalN = firstHalfOfN + secondHalfOfN - 1; // 1-based index
 
-   int finalN = firstHalfOfN + secondHalfOfN;
    if (methodOfAddition == 'i') { // iterative
-      int sumOfFibonacci = 0;
-      int previousNum = 0;
-      int currentNum = 1;
-      for (int indexOfLoop = 0; indexOfLoop < finalN; ++indexOfLoop) {
-         sumOfFibonacci += previousNum + currentNum;
-         previousNum = currentNum;
-         currentNum = sumOfFibonacci;
-      }
+      int sumOfFibonacci = fibonacciIterative(finalN);
       printf("%d", sumOfFibonacci);
-      return sumOfFibonacci;
+      return 0;
    } else { // recursive
       int sumOfFibonacci = fibonacciRecursive(finalN);
       printf("%d", sumOfFibonacci);
-      return sumOfFibonacci;
+      return 0;
    }
 }

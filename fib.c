ceaac8201f136cfdad2de838c62aa967fe43f3ac
Keith Bui <kqbui@csu.fullerton.edu>
2024-09-03 19:30:01 -0700

    first attempt at reading files using c; local tests aren't working so trying github classroom to see if things are any different... or if it's a logic error

--- fib.c
+++ fib.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main() {
-   printf("Hello!\n");
-   return 0;
+int fibonacciRecursive(int recursionValue) {
+   if (recursionValue == 0 || recursionValue == 1) {
+      return recursionValue;
+   }
+   return (fibonacciRecursive(recursionValue - 2) + fibonacciRecursive(recursionValue - 1));
+}
+
+int main(int argc, char *argv[]) {
+   int firstHalfOfN = *argv[1];
+   int secondHalfOfN;
+   char methodOfAddition = *argv[2];
+   char *fileName = argv[3];
+
+   FILE * fibonacciFile = fopen(fileName, "r");
+   fscanf(fibonacciFile, "%u", &secondHalfOfN);
+   
+   int finalN = firstHalfOfN + secondHalfOfN;
+   if (methodOfAddition == 'i') { // iterative
+      int sumOfFibonacci = 0;
+      int previousNum = 0;
+      int currentNum = 1;
+      for (int indexOfLoop = 0; indexOfLoop < finalN; ++indexOfLoop) {
+         sumOfFibonacci += previousNum + currentNum;
+         previousNum = currentNum;
+         currentNum = sumOfFibonacci;
+      }
+      return sumOfFibonacci;
+   } else { // recursive
+      return fibonacciRecursive(finalN);
+   }
 }
